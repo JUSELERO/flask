fa2367f5798d22b16fa35f3f80c7dc79df39a959
juselero <juansebastianleon5@gmail.com>
2020-03-24 23:11:11 -0500

    por defecto

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
 
 app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'juse'
-app.config['MYSQL_PASSWORD'] = 'admin'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'JuSeTech'
 
 
@@ -61,7 +61,7 @@ def Ventas_echa():
     data=cur.fetchall()
     return render_template('ventas.html' , Ventas = data)
 
-@app.route('/productos' , methods = ['POST','GET'] )
+@app.route('/productos', methods = ['POST','GET'] )
 def Productos():
     if request.method == 'POST':
         nombre = request.form['nombre']        
